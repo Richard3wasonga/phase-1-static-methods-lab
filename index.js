982590d28a25d616a7d06b6e3cedd30e616541a3
richard <richard.wasonga@student.moringaschool.com>
2025-04-04 17:00:35 +0300

    complete lab

--- index.js
+++ index.js
@@ -1,3 +1,22 @@
 class Formatter {
   //add static methods here
+  static capitalize(string){
+    return string.charAt(0).toUpperCase() + string.slice(1)
+  }
+  static sanitize(string){
+    return string.replace(/[^a-z0-9\-\'\s]/gi, '')
+  }
+  static titleize(string){
+    const except = ['the', 'a', 'an', 'but', 'of', 'and', 'for', 'at', 'by', 'from']
+    const words = string.split(" ")
+    const upperwords = words.map((word,index) => {
+      if(except.includes(word) && index !== 0){
+        return word.toLowerCase()
+      }else{
+        return Formatter.capitalize(word)
+      }
+    })
+    return upperwords.join(" ")
+   
+  }
 }
